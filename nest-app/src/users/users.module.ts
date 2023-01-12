a3302f587fb502fabb506e274640122d9819da0d
Sarah Solarin <solarinsarah3@gmail.com>
2023-01-12 07:18:57 +0000

    Added mongo docker connection string

--- nest-app/src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { User, UserShema } from "./schemas/user.schema";
-
-@Module({
-    imports: [MongooseModule.forFeature([ { name: User.name, schema: UserShema } ])]
-})
-
-export class UserModule {}
